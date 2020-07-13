d4470bd348f4a77e126c8aa9011e50ede32df43f
Sol-Mayers <rinotello@yandex.ru>
2020-07-14 00:21:09 +0300

    Доделал отображение метки

--- js/main.js
+++ js/main.js
@@ -119,8 +119,7 @@ var adForm = document.querySelector('.ad-form');
 var adress = document.querySelector('#address');
 
 var mainAdress = function () {
-  adress.value = 'X:' + parseInt(mainPin.style.left, 10) + ' Y:' + parseInt(mainPin.style.top, 10);
-// проработать правильное отображение координат за вычетом размеров метки!!!
+  adress.value = 'X:' + (parseInt(mainPin.style.left, 10) + 156/2)+ ' Y:' + (parseInt(mainPin.style.top, 10) + 156/2);
 };
 mainAdress();
 
@@ -129,12 +128,16 @@ mainPin.addEventListener('mousedown', function (touch) {
     map.classList.remove('map--faded');
     createSetOfItems(setOfSuggestions);
     mapItems.appendChild(fragment);
+    var activeAdress = function () {
+      adress.value = 'X:' + (parseInt(mainPin.style.left, 10) + 156/2)+ ' Y:' + (parseInt(mainPin.style.top, 10) + (156/2) + 47);
+    };
     var enableFieldsets = function () {
       document.querySelectorAll('fieldset').forEach(function (element) {
         element.removeAttribute('disabled', 'disabled');
       });
     };
     enableFieldsets();
+    activeAdress();
     adForm.classList.remove('ad-form--disabled');
   }
 });
